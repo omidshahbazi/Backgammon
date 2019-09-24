679b5eb1154928ec3b34fa9441a3dd0fd297f398
Omid Shahbazi <sh.omid.m@gmail.com>
2019-09-24 21:38:19 +0330

    Add        Project/Simulation/Common/Serialization/Serializer.cs Add        Project/Simulation/Data/Serialization/Deserializer.cs Add        Project/Simulation/Data/Serialization/DiffFinder.cs Add        Project/Simulation/Data/Serialization/SerializerVisitor.cs Add        Project/Simulation/Data/Serialization/SessionDeserializer.cs Add        Project/Simulation/Data/Serialization/SessionSerializer.cs

---
+++ Project/Simulation/Data/Serialization/Deserializer.cs
@@ -0,0 +1,120 @@
+﻿using Simulation.Common.Serialization;
+using Simulation.Data.Event;
+using Simulation.Data.Game;
+using System;
+using System.IO;
+
+namespace Simulation.Data.Serialization
+{
+	public static class Deserializer
+	{
+		public static FrameData DeserializeFrameData(byte[] Data)
+		{
+			Serializer serializer = new Serializer(new MemoryStream(Data, 0, Data.Length, false, true));
+
+			return DeserializeFrameData(serializer);
+		}
+
+		public static BoardData DeserializeBoardData(byte[] Data)
+		{
+			Serializer serializer = new Serializer(new MemoryStream(Data, 0, Data.Length, false, true));
+
+			return DeserializeBoardData(serializer);
+		}
+
+		public static PointData DeserializePointData(byte[] Data)
+		{
+			Serializer serializer = new Serializer(new MemoryStream(Data, 0, Data.Length, false, true));
+
+			return DeserializePointData(serializer);
+		}
+
+		public static EventBase[] DeserializeEvents(byte[] Data)
+		{
+			Serializer serializer = new Serializer(new MemoryStream(Data, 0, Data.Length, false, true));
+
+			return DeserializeEventsData(serializer);
+		}
+
+		public static FrameData DeserializeFrameData(Serializer Serializer)
+		{
+			FrameData data = new FrameData();
+
+			data.Board = DeserializeBoardData(Serializer);
+
+			data.Events = DeserializeEventsData(Serializer);
+
+			return data;
+		}
+
+		public static EventBase[] DeserializeEventsData(Serializer Serializer)
+		{
+			int len = Serializer.BeginReadArray();
+
+			EventBase[] events = new EventBase[len];
+
+			for (int i = 0; i < len; ++i)
+				events[i] = DeserializeEventBase(Serializer);
+
+			return events;
+		}
+
+		public static EventBase DeserializeEventBase(Serializer Serializer)
+		{
+			EventBase.Types type = (EventBase.Types)Serializer.ReadInt32();
+
+			switch (type)
+			{
+				case EventBase.Types.Move:
+					return DeserializeMoveEvent(Serializer);
+
+				default:
+					throw new Exception("Unsupported Type");
+			}
+		}
+
+		private static void DeserializeDataBase(Serializer Serializer, DataBase Data)
+		{
+		}
+
+		private static MoveEvent DeserializeMoveEvent(Serializer Serializer)
+		{
+			MoveEvent data = new MoveEvent();
+
+
+			return data;
+		}
+
+		public static BoardData DeserializeBoardData(Serializer Serializer)
+		{
+			BoardData data = new BoardData();
+
+			DeserializeDataBase(Serializer, data);
+
+			int len = Serializer.BeginReadArray();
+			data.Points = new PointData[len];
+
+			for (int i = 0; i < len; ++i)
+				data.Points[i] = DeserializePointData(Serializer);
+
+			data.OnBarWhiteCheckerCount = Serializer.ReadInt32();
+			data.OnBarBlackCheckerCount = Serializer.ReadInt32();
+			data.BearedOffWhiteCheckersCount = Serializer.ReadInt32();
+			data.BearedOffBlackCheckersCount = Serializer.ReadInt32();
+
+			return data;
+		}
+
+		public static PointData DeserializePointData(Serializer Serializer)
+		{
+			PointData data = new PointData();
+
+			DeserializeDataBase(Serializer, data);
+
+			data.CheckerCount = Serializer.ReadInt32();
+			data.Color = (PointData.Colors)Serializer.ReadInt32();
+
+			return data;
+		}
+	}
+}
---
+++ Project/Simulation/Data/Serialization/SerializerVisitor.cs
@@ -0,0 +1,63 @@
+﻿using Simulation.Common;
+using Simulation.Common.Serialization;
+using Simulation.Common.Visitor;
+using System.Collections;
+using System.IO;
+
+namespace Simulation.Data.Serialization
+{
+	public class SerializerVisitor : IVisitor
+	{
+		private Serializer serializer = null;
+
+		public byte[] Data
+		{
+			get { return serializer.Data.ToArray(); }
+		}
+
+		public SerializerVisitor()
+		{
+			Reset();
+		}
+
+		public void Reset()
+		{
+			serializer = new Serializer(new MemoryStream());
+		}
+
+		public void BeginVisitArray(ICollection Collection)
+		{
+			serializer.BeginWriteArray(Collection == null ? 0 : Collection.Count);
+		}
+
+		public void EndVisitArray()
+		{
+			serializer.EndWriteArray();
+		}
+
+		public void BeginVisitArrayElement()
+		{
+			serializer.BeginWriteArrayElement();
+		}
+
+		public void EndVisitArrayElement()
+		{
+			serializer.EndWriteArrayElement();
+		}
+
+		public void VisitBool(bool Bool)
+		{
+			serializer.WriteBool(Bool);
+		}
+
+		public void VisitInt32(int Int)
+		{
+			serializer.WriteInt32(Int);
+		}
+
+		public void VisitIdentifier(Identifier Identifier)
+		{
+			VisitInt32(Identifier);
+		}
+	}
+}
