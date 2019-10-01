41bb12a848893a2957927e66c763277df5ac0aba
neo <neoparmin@live.com>
2019-10-01 09:53:17 +0330

    -minor change

---
+++ Project/Client/Assets/Scripts/ClientUtilities/UI/ViewportHandler.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class ViewportHandler : MonoBehaviour
+{
+
+    public Vector2 ReferenceRatio = new Vector2(9f, 16f);
+    private float fixedOthographicSize = 5.6F;
+    private Camera camera;
+
+    private void Awake()
+    {
+        camera = GetComponent<Camera>();
+
+        ComputeCameraBound();
+    }
+
+    private void ComputeCameraBound()
+    {
+
+        float  num1= Screen.width / (float)Screen.height;
+        float num2 = this.ReferenceRatio.x / this.ReferenceRatio.y;
+
+        float num3 = num2 / num1;
+
+        if (num1 >= num2)
+            return;
+
+        camera.orthographicSize = fixedOthographicSize* num3;
+       
+    }
+    
+}
