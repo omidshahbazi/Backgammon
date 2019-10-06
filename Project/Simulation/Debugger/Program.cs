22f22ac02219c8df66a2e6053717070340a6bc7e
Omid Shahbazi <sh.omid.m@gmail.com>
2019-10-06 23:46:44 +0330

    Modified   Project/Simulation/Simulation.sln Add        Project/Simulation/Debugger/Debugger.csproj Add        Project/Simulation/Debugger/Program.cs Add        Project/Simulation/Debugger/Properties/AssemblyInfo.cs Add        Project/Simulation/Debugger/app.config

---
+++ Project/Simulation/Debugger/Program.cs
@@ -0,0 +1,26 @@
+﻿using Simulation.Data.Game;
+using Simulation.Data.Serialization;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Simulation.Debugger
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SessionDeserializer deserializer = new SessionDeserializer(File.ReadAllBytes("D:/dump.bin"));
+
+			ConfigData config = deserializer.DeserializeConfigDataState();
+			FrameData frame = deserializer.DeserializeInitialState();
+
+			List<FrameData> frames = new List<FrameData>();
+
+			FrameData stepFrame = null;
+			while ((stepFrame = deserializer.DeserializeFullStep()) != null)
+				frames.Add(frame);
+
+
+		}
+	}
+}
