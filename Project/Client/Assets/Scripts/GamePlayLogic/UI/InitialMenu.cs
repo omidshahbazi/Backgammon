25ee3e1fc81af049acd0807cb19162a86ff048d5
neo <neoparmin@live.com>
2019-10-19 23:27:56 +0330

    -minor change in ui

---
+++ Project/Client/Assets/Scripts/GamePlayLogic/UI/InitialMenu.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Scripts.GamePlayLogic.RequestManagers;
+using Networking.Common;
+using Assets.Scripts.ClientUtilities.ScheduleSystem;
+using Assets.Scripts.ClientUtilities.Extensions;
+using DG.Tweening;
+
+namespace Assets.Scripts.GamePlayLogic.UI
+{
+
+    public class InitialMenu : UIBase
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+
+            RegisterUI("InitialMenu", this);
+        }
+
+    }
+}
