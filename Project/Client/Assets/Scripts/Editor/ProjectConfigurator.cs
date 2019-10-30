a62ffd2584100c8c4bad25048a7e42d12a67104f
Omid Shahbazi <sh.omid.m@gmail.com>
2019-10-30 19:21:44 +0330

    Modified   Project/Client/Assets/Plugins/Android/AndroidManifest.xml Modified   Project/Client/Assets/Scripts/Editor/ProjectConfigurator.cs Add        Project/Client/Assets/Plugins/Android/AndroidManifest.Template.xml Add        Project/Client/Assets/Plugins/Android/AndroidManifest.Template.xml.meta

--- Project/Client/Assets/Scripts/Editor/ProjectConfigurator.cs
+++ Project/Client/Assets/Scripts/Editor/ProjectConfigurator.cs
@@ -2,11 +2,17 @@
 using Assets.Scripts.GamePlayLogic;
 using Networking.Common;
 using System;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
 public static class ProjectConfigurator
 {
+	private const string MARKET_ACTIVITY_PLACEHOLDER = "<MARKET_ACTIVITY/>";
+	private const string MARKET_PERMISSION_PLACEHOLDER = "<MARKET_PERMISSION/>";
+	private const string MYKET_ACTIVITY = "<activity android:name=\"ir.myket.unity.iab.MyketBillingService$IabActivity\" android:theme=\"@android:style/Theme.Translucent.NoTitleBar.Fullscreen\" android:configChanges=\"orientation|screenSize|keyboardHidden\"/>";
+	private const string MYKET_PERMISSION = "<uses-permission android:name=\"ir.mservices.market.BILLING\"/>";
+
 	public static void Configure(Markets Market)
 	{
 		GameObject projectConfigsObj = Resources.Load<GameObject>("Prefabs/ProjectConfigs");
@@ -45,6 +51,10 @@ public static class ProjectConfigurator
 		PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Standalone, Constants.PACKAGE_NAME);
 		PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, Constants.PACKAGE_NAME);
 
+		string defines = "MARKET_" + Market.ToString().ToUpper();
+		PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, defines);
+		PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android, defines);
+
 		PlayerSettings.Android.bundleVersionCode = versionNumber;
 
 		PlayerSettings.Android.useCustomKeystore = true;
@@ -52,6 +62,22 @@ public static class ProjectConfigurator
 		PlayerSettings.Android.keystorePass = "#EDC2wsx";
 		PlayerSettings.Android.keyaliasName = "royalgammon";
 		PlayerSettings.Android.keyaliasPass = "#EDC2wsx";
+
+		string templateManifest = File.ReadAllText(Application.dataPath + "/Plugins/Android/AndroidManifest.Template.xml");
+
+		switch (Market)
+		{
+			case Markets.Windows:
+				break;
+			case Markets.Cafebazaar:
+				break;
+			case Markets.Myket:
+				templateManifest = templateManifest.Replace(MARKET_ACTIVITY_PLACEHOLDER, MYKET_ACTIVITY);
+				templateManifest = templateManifest.Replace(MARKET_PERMISSION_PLACEHOLDER, MYKET_PERMISSION);
+				break;
+		}
+
+		File.WriteAllText(Application.dataPath + "/Plugins/Android/AndroidManifest.xml", templateManifest);
 	}
 
 	[MenuItem("Edit/Configure Project Settings (Myket)")]
