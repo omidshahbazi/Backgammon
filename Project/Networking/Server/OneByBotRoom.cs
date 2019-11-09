fa3a48f2000a678cd9673029fa6cb1bc3dc14854
Omid Shahbazi <sh.omid.m@gmail.com>
2019-11-09 20:41:53 +0330

    Modified   Project/Client/Assets/Libraries/Networking.Client.dll Modified   Project/Networking/Server/OneByBotRoom.cs Modified   Project/Networking/Server/OneByOneRoom.cs Modified   Project/Networking/Server/Room.cs Modified   Project/Simulation/Debugger/Program.cs

--- Project/Networking/Server/OneByBotRoom.cs
+++ Project/Networking/Server/OneByBotRoom.cs
@@ -1,4 +1,5 @@
-﻿using Networking.Common;
+﻿//#define BOT_IS_BLACK
+using Networking.Common;
 using Simulation.Data.Game;
 using Simulation.Logic;
 using GameFramework.ASCIISerializer;
@@ -12,12 +13,20 @@ namespace Networking.Server
 
 		protected override Player WhitePlayer
 		{
+#if BOT_IS_BLACK
 			get { return (PLayerCount == 1 ? Players[0] : null); }
+#else
+			get { return null; }
+#endif
 		}
 
 		protected override Player BlackPlayer
 		{
+#if BOT_IS_BLACK
 			get { return null; }
+#else
+			get { return (PLayerCount == 1 ? Players[0] : null); }
+#endif
 		}
 
 		public override string BotPlayerInfo
@@ -32,7 +41,11 @@ namespace Networking.Server
 
 		public override void Initialize()
 		{
+#if BOT_IS_BLACK
 			ISerializeObject obj = BotPlayerInfoMaker.Make(WhitePlayer.ID);
+#else
+			ISerializeObject obj = BotPlayerInfoMaker.Make(BlackPlayer.ID);
+#endif
 
 			if (obj != null)
 				botPlayerInfo = obj.Content;
@@ -42,7 +55,20 @@ namespace Networking.Server
 
 		protected override int CreateGame()
 		{
+#if BOT_IS_BLACK
 			return DatabaseLayer.CreateGame(DatabaseLayer.GameTypes.OneByBot, Bet, WhitePlayer.Version);
+#else
+			return DatabaseLayer.CreateGame(DatabaseLayer.GameTypes.OneByBot, Bet, BlackPlayer.Version);
+#endif
+		}
+
+		protected override void InitializeGame()
+		{
+#if BOT_IS_BLACK
+			DatabaseLayer.InitializeGame(GameID, WhitePlayer.ID, Constants.NULL_USER_ID, BotPlayerInfo);
+#else
+			DatabaseLayer.InitializeGame(GameID, Constants.NULL_USER_ID, BlackPlayer.ID, BotPlayerInfo);
+#endif
 		}
 
 		protected override void HandleGetGameData(Player Player)
@@ -54,14 +80,22 @@ namespace Networking.Server
 			SendBuffer.Reset();
 			SendBuffer.WriteBytes(Commands.Category.ROOM, Commands.Room.GET_GAME_DATA);
 
+#if BOT_IS_BLACK
 			SendBuffer.WriteInt32((int)PlayerColors.White);
+#else
+			SendBuffer.WriteInt32((int)PlayerColors.Black);
+#endif
 
 			Send(Player, SendBuffer);
 		}
 
 		protected override void ScheduleCheckTurnTime()
 		{
+#if BOT_IS_BLACK
 			if (Simulator.Frame.Board.TurnColor == PlayerColors.Black)
+#else
+			if (Simulator.Frame.Board.TurnColor == PlayerColors.White)
+#endif
 			{
 				float actTime = Configs.Random.Next(4, TurnTime);
 
