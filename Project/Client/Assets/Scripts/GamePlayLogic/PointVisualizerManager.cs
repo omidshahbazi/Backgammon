d4c38a022ac6ab95711f37f240a28b15bca70577
neo <neoparmin@live.com>
2019-11-19 22:43:58 +0330

    -minor change

--- Project/Client/Assets/Scripts/GamePlayLogic/PointVisualizerManager.cs
+++ Project/Client/Assets/Scripts/GamePlayLogic/PointVisualizerManager.cs
@@ -241,6 +241,7 @@ namespace Assets.Scripts.GamePlayLogic
             bd.Trail.enabled = true;
             if (LeanTween.isTweening(bd.gameObject))
                 LeanTween.cancel(bd.gameObject);
+
             LeanTween.move(bd.gameObject, extraBar.FindPosition(extraBar.pointBeeds.Count - 1), 0.8F).setEase(LeanTweenType.easeInOutSine).setOnComplete(() =>
             {
                 bd.Trail.enabled = false;
