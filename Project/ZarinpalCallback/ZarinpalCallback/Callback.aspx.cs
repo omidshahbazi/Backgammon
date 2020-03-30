78efa0d4f4401ccb334b91d69070f53e34bf0c1f
Omid Shahbazi <sh.omid.m@gmail.com>
2020-03-30 12:41:06 +0430

    Added      Project/ZarinpalCallback/ZarinpalCallback.sln Added      Project/ZarinpalCallback/ZarinpalCallback/Callback.aspx Added      Project/ZarinpalCallback/ZarinpalCallback/Callback.aspx.cs Added      Project/ZarinpalCallback/ZarinpalCallback/Callback.aspx.designer.cs Added      Project/ZarinpalCallback/ZarinpalCallback/Properties/AssemblyInfo.cs Added      Project/ZarinpalCallback/ZarinpalCallback/Web.Debug.config Added      Project/ZarinpalCallback/ZarinpalCallback/Web.Release.config Added      Project/ZarinpalCallback/ZarinpalCallback/Web.config Added      Project/ZarinpalCallback/ZarinpalCallback/ZarinpalCallback.csproj Modified   Project/Networking/Binaries/Resources/InitialData_Default.json

---
+++ Project/ZarinpalCallback/ZarinpalCallback/Callback.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ZarinpalCallback
+{
+	public partial class Callback : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			string result = Request.QueryString["result"];
+
+			if (string.IsNullOrEmpty(result))
+				return;
+
+			Response.Redirect(result);
+		}
+	}
+}
